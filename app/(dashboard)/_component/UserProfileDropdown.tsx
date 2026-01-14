e48645478d18d230a1c3f9a17b7f6e41b1fcc728
wesley pagnol <emperorb007@gmail.com>
2026-01-14 18:16:27 +0100

    chart page complete

--- app/(dashboard)/_component/UserProfileDropdown.tsx
+++ app/(dashboard)/_component/UserProfileDropdown.tsx
@@ -9,63 +9,59 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { auth, signOut } from "@/lib/auth";
 import getAvatarName from "@/lib/getAvatarName";
-import { ChevronDown, ChevronUp, User2 } from "lucide-react";
+import { ChevronDown } from "lucide-react";
 import UserProfile from "./UserProfile";
-import { SidebarMenuButton } from "@/components/ui/sidebar";
 
 interface IUserProfileDropdown {
   isFullName: boolean;
   isArrowUp: boolean;
 }
 
-export default async function UserProfileDropdown({
+export default async function UserProfileDropDown({
   isFullName,
   isArrowUp,
 }: IUserProfileDropdown) {
   const session = await auth();
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
-        <div className="flex items-center space-x-2 p-4 hover:bg-gray-100 rounded-md cursor-pointer">
-          <Avatar className="border size-9 bg-neutral-30 cursor-pointer">
-            <AvatarImage src={session?.user?.image as string} />
+      <DropdownMenuTrigger asChild>
+        <div className="flex items-center gap-3 cursor-pointer">
+          <Avatar className="border size-9 bg-neutral-300 cursor-pointer">
+            <AvatarImage src={session?.user.image as string} />
             <AvatarFallback>
               {getAvatarName(
-                session?.user?.firstName as string,
-                session?.user?.lastName as string | null
+                session?.user.firstName as string,
+                session?.user.lastName as string
               )}
             </AvatarFallback>
           </Avatar>
           {isFullName && (
             <div>
               <p className="text-ellipsis line-clamp-1 font-medium">
-                <span>{session?.user?.firstName}</span>{" "}
-                <span>{session?.user?.lastName}</span>
+                <span>{session?.user.firstName}</span>{" "}
+                <span>{session?.user.lastName}</span>
               </p>
             </div>
           )}
 
-          {isArrowUp && (
-            <ChevronDown size={16} className="transition-all ml-auto" />
-          )}
+          {isArrowUp && <ChevronDown className="transition-all ml-auto" />}
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-full min-w-[250px]">
-        <DropdownMenuLabel>Mon Compte</DropdownMenuLabel>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        {/* user profile */}
+        {/**user profile */}
         <UserProfile />
         <DropdownMenuItem
           onClick={async () => {
             "use server";
             await signOut();
           }}
-          className="bg-red-50 text-red-500 hover:bg-red-100 font-medium"
+          className="bg-red-50 text-red-500 hover:bg-red-100 font-medium cursor-pointer"
         >
-          Déconnexion
+          Logout
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-
   );
 }
