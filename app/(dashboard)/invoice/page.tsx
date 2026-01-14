2652c7314e00b87684e4d76e367d50c2b9e20942
wesley pagnol <emperorb007@gmail.com>
2026-01-14 17:15:49 +0100

    invoice page complete

--- app/(dashboard)/invoice/page.tsx
+++ app/(dashboard)/invoice/page.tsx
@@ -1,35 +1,15 @@
-"use client"
+import Loading from "@/components/Loading";
+import { auth } from "@/lib/auth";
+import { Suspense } from "react";
+import InvoiceClientPage from "../_component/InvoiceClientPage";
 
-import { buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
-
-export default function InvoicePage(){
-    return (
-        <div className="p-4">
-            <div className="flex items-center justify-between gap-4">
-                <h1 className="text-xl font-semibold">Factures</h1>
-                <Link href={"/invoice/create"} className={cn(buttonVariants(), "cursor-pointer")}>
-                    Créer une facture
-                </Link>
-            </div>
-        </div>
+export default async function InvoicePage(){
+    const session = await auth()
+    return(
+        <Suspense 
+        fallback={<Loading/>}
+        >
+            <InvoiceClientPage userId={session?.user.id} currency={session?.user.currency}/>
+        </Suspense>
     )
 }
-
-// import { buttonVariants } from "@/components/ui/button";
-// import { auth } from "@/lib/auth";
-// import { cn } from "@/lib/utils";
-// import Link from "next/link";
-// import { Suspense } from "react";
-
-// export default async function InvoicePage(){
-//     const session = await auth()
-//     return(
-//         <Suspense 
-//         // fallback={<Loading/>}
-//         >
-//             {/* <InvoiceClientPage userId={session?.user.id} currency={session?.user.currency}/> */}
-//         </Suspense>
-//     )
-// }
